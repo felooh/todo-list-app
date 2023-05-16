f7468700a63e2a7b7ab8341000970572a4b32ef2
felix <gathagefelix@gmail.com>
2023-05-16 08:24:17 +0300

    first commit since repository creation

---
+++ todolist/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    content = models.TextField()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
